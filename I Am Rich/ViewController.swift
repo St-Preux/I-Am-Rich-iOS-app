deceb3e16c7f965e21772f616907b4671cbc1a3d
St-Preux <ty@tylers-mbp-2.domain>
2018-05-14 14:41:42 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Tyler St.Preux on 5/13/18.
+//  Copyright © 2018 Tyler St.Preux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
